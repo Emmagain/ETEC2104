797fa633211e621789ca277d683a3055a764e49e
Emmagain <emma.l.brandt3@gmail.com>
2025-02-17 10:48:49 -0500

    DOM lab finished

--- html/signup.js
+++ html/signup.js
@@ -7,10 +7,16 @@ function check(){
     let n = input.value;
     input = document.getElementById("password");
     let p = input.value;
+
+    //New Alert System
+    let tmp = document.createElement("div");
+    tmp.classList.add("alert");
     
     //Fill-in check
     if ( p.length == 0 || dob.length == 0 || e.length == 0 || n.length == 0 ) {
-        alert("You have not filled out all boxes");
+        //alert("You have not filled out all boxes");
+        document.body.appendChild(tmp);
+        tmp.appendChild(document.createTextNode("You have not filled out all necessary information."));
         return
     }
     
@@ -19,20 +25,26 @@ function check(){
     let d = birthday.getTime();
     let day = Date.now();
     if (d > day - 410248800000) {
-        alert("Invalid age");
+        //alert("Invalid age");
+        document.body.appendChild(tmp);
+        tmp.appendChild(document.createTextNode("Invalid age."));
         return
     }
     
     //Email check
     let first = e.indexOf("@");
     if (first == -1 || first == 0 || first == e.length - 1) {
-        alert("Invalid email");
+       // alert("Invalid email");
+       document.body.appendChild(tmp);
+        tmp.appendChild(document.createTextNode("Invalid email."));
         return
     }
     let second = e.indexOf("@", first + 1);
     if (second > 0) {
-        alert("Invalid email");
+        //alert("Invalid email");
+        document.body.appendChild(tmp);
+        tmp.appendChild(document.createTextNode("Invalid email."));
         return
     }
-    
+        
 }
